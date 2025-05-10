9fe7e0ff8a1de9c295e5f7bc502c6916525daa91
Irene Riportella <riportellairene@gmail.com>
2025-05-10 21:37:40 +0200

    feat:🎉 first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+      <h1 className="text-center text-4xl mt-8 mb-16 font-bold">
+        Project 7: BMI Calculator
+      </h1>
+      <div className="flex flex-col items-center max-w-2xl mx-auto">
+
+
+      </div>
+    </>
+  )
+}
+
+export default App
